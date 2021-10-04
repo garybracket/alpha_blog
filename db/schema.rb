2dedf3567e7b30a367dddd56eb807a2d6206fac6
Gary McQueen <garybracket2@gmail.com>
2021-10-03 21:01:56 -0400

    last of it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_204337) do
+ActiveRecord::Schema.define(version: 2021_10_04_010120) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
