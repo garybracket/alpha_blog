15655d3a5a298e6155e512dc25def2e20b63f629
Gary McQueen <garybracket@hotmail.com>
2022-06-27 19:47:41 -0400

    user authentication and association with articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,5 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img-fluid, shadow-lg")
   end
+
 end
